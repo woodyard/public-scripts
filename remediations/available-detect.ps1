3fac697641a42fb537de0ffd41f2cf7d9eca5ef1
Henrik Skovgaard <hnrk@skvgrd.com>
2023-09-22 12:58:16 +0200

    exit codes

--- remediations/available-detect.ps1
+++ remediations/available-detect.ps1
@@ -1,1 +1,2 @@
 iex (irm "https://raw.githubusercontent.com/woodyard/public-scripts/main/remediations/availableUpgrades-detect.ps1")
+$exitCode = $LASTEXITCODE
--- remediations/available-remediate.ps1
+++ remediations/available-remediate.ps1
@@ -1,1 +1,2 @@
 iex (irm "https://raw.githubusercontent.com/woodyard/public-scripts/main/remediations/availableUpgrades-remediate.ps1")
+$exitCode = $LASTEXITCODE
