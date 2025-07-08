abfdb847532b243bd4828d57ec95db17e7612e14
Henrik Skovgaard <hnrk@skvgrd.com>
2025-07-08 14:00:00 +0200

    Sync

--- remediations/available-detect-all.ps1
+++ remediations/available-detect-all.ps1
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 iex (irm "https://raw.githubusercontent.com/woodyard/public-scripts/main/remediations/availableUpgrades-detect-all.ps1")
-=======
-iex (irm "https://raw.githubusercontent.com/woodyard/public-scripts/main/remediations/detect-all.ps1")
->>>>>>> b8600d1d838a02bbcbf4acbeb66d14e81b92e8cc
 $exitCode = $LASTEXITCODE
